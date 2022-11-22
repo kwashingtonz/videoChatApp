de90adb2186bbe5e39e0ac6fdf5beeb198838489
kwashingtonz <kalinduwashington@gmail.com>
2022-11-22 17:01:50 +0530

    modified

--- client/src/views/signup_component.js
+++ client/src/views/signup_component.js
@@ -11,6 +11,7 @@ export default class SignUp extends Component {
       lname: "",
       email: "",
       password: "",
+      cpassword: "",
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -24,62 +25,68 @@ export default class SignUp extends Component {
       theme: "dark",
     };
     e.preventDefault();
-    const { fname, lname, email, password } = this.state;
+    const { fname, lname, email, password, cpassword } = this.state;
     console.log(fname, lname, email, password);
 
-    fetch("http://localhost:8000/user/register", {
-      method: "POST",
-      crossDomain: true,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        fname,
-        email,
-        lname,
-        password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
+    if(password !== cpassword){
+      toast.error("Passwords Do not match!", toastOptions);
+    }
+    else{
 
-        if (data.status === "ok") {
-          //alert("Registration Successful");
-          toast.success("signup successful", toastOptions);
-          
-          //window.location.href = "/";
-          window.setTimeout(function() {
-            window.location.href = '/';
-        }, 3000);
+      fetch("http://localhost:8000/user/register", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          fname,
+          email,
+          lname,
+          password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
 
-        }else if(fname === "" || lname ==="" || email === "" || password === ""){
-          toast.error("All fields must be filled", toastOptions);
-          
-        // eslint-disable-next-line  
-       } else if (!/^[\w+\.]+@([\w+]+\.)+[\w-]{2,4}$/.test(email)){
-         toast.error("please provide a valide email", toastOptions);
+          if (data.status === "ok") {
+            //alert("Registration Successful");
+            toast.success("signup successful", toastOptions);
+            
+            //window.location.href = "/";
+            window.setTimeout(function() {
+              window.location.href = '/';
+          }, 3000);
 
-       }else if (!/^[a-zA-Z]*$/.test(fname)){
-         toast.error("first name must be charactors", toastOptions);
+          }else if(fname === "" || lname ==="" || email === "" || password === ""){
+            toast.error("All fields must be filled", toastOptions);
+            
+          // eslint-disable-next-line  
+        } else if (!/^[\w+\.]+@([\w+]+\.)+[\w-]{2,4}$/.test(email)){
+          toast.error("please provide a valide email", toastOptions);
 
-       }else if (!/^[a-zA-Z]*$/.test(lname)){
-         toast.error("last name must be charactors", toastOptions);
+        }else if (!/^[a-zA-Z]*$/.test(fname)){
+          toast.error("first name must be charactors", toastOptions);
 
-       }else if (password.length < 8){
-         toast.error("passowrd must be 8 charactors", toastOptions);
+        }else if (!/^[a-zA-Z]*$/.test(lname)){
+          toast.error("last name must be charactors", toastOptions);
 
-       }else if (email.match){
-         toast.error("Already User registered!", toastOptions);
-       }
-        // } else if (data.status === "User exists") {
-        //   alert("User exists");
-        // } else if(data.status === "error"){
-        //   alert("error");
-        // }
+        }else if (password.length < 8){
+          toast.error("passowrd must be 8 charactors", toastOptions);
 
-      });
+        }else if (email.match){
+          toast.error("Already User registered!", toastOptions);
+        }
+          // } else if (data.status === "User exists") {
+          //   alert("User exists");
+          // } else if(data.status === "error"){
+          //   alert("error");
+          // }
+
+        });
+    }
   }
   render() {
     return (
@@ -117,6 +124,14 @@ export default class SignUp extends Component {
             placeholder="Enter password"
             onChange={(e) => this.setState({ password: e.target.value })}
           />
+
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter confirm password"
+            onChange={(e) => this.setState({ cpassword: e.target.value })}
+          />
+
 
           <button type="submit">
             Sign Up
